c2780d9e5ba4143f2d69654486f1ffd3ecffcd84
Peawills <115140417+Peawills@users.noreply.github.com>
2025-07-14 15:22:48 +0100

    Search filter

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -6,4 +6,5 @@ admin.register(Book)
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
+    list_filter = ('publication_year', 'author')
     search_fields = ('title', 'author')
