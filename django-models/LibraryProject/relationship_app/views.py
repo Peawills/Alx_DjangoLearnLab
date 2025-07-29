af39e0f052b0ef6b0186b67a601d4ec85a2d1fe0
Peawills <115140417+Peawills@users.noreply.github.com>
2025-07-29 21:35:45 +0100

    fix a

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,15 +1,7 @@
-from django.shortcuts import render
-from django.views.generic import DetailView
-from .models import Book, Library
+from django.views.generic.detail import DetailView
+from .models import Library
 
 
-# Function-based view
-def list_books(request):
-    books = Book.objects.all()
-    return render(request, "relationship_app/list_books.html", {"books": books})
-
-
-# Class-based view
 class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
