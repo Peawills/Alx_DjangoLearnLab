59f98a085705e87d3d391b346c11602caa9c4fec
Peawills <115140417+Peawills@users.noreply.github.com>
2025-07-30 11:00:48 +0100

    User Auth

--- django-models/LibraryProject/LibraryProject/settings.py
+++ django-models/LibraryProject/LibraryProject/settings.py
@@ -121,3 +121,9 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# LibraryProject/settings.py
+
+LOGIN_REDIRECT_URL = "/"  # or 'home' if using named URLs
+LOGOUT_REDIRECT_URL = "/login/"
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.detail import DetailView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 from .models import Library
 from .models import Book
+
+
+
+def register_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home")  # replace 'home' with the name of your landing view
+    else:
+        form = UserCreationForm()
+    return render(request, "relationship_app/register.html", {"form": form})
 
 
 # Function-based view to list all books
