9cb79294e8f297b45ad516457fb54355038fb811
Peawills <115140417+Peawills@users.noreply.github.com>
2025-08-30 08:10:32 +0100

    Cheacker

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -11,8 +11,8 @@ urlpatterns = [
     path("books/", BookListView.as_view(), name="book-list"),  # GET all
     path("books/<int:pk>/", BookDetailView.as_view(), name="book-detail"),  # GET one
     path("books/create/", BookCreateView.as_view(), name="book-create"),  # POST
-    path("books/<int:pk>/update/", BookUpdateView.as_view(), name="book-update"),  # PUT
+    path("books/update/<int:pk>/", BookUpdateView.as_view(), name="book-update"),  # PUT
     path(
-        "books/<int:pk>/delete/", BookDeleteView.as_view(), name="book-delete"
+        "books/delete/<int:pk>/", BookDeleteView.as_view(), name="book-delete"
     ),  # DELETE
 ]
