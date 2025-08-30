a77ac1afc4bb8f7e8defb4077ed851e1f88b9b7b
Peawills <115140417+Peawills@users.noreply.github.com>
2025-08-30 13:03:51 +0100

    user

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib import messages
+from .forms import UserRegisterForm, UserUpdateForm
 
-# Create your views here.
+
+# Registration view
+def register(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # auto-login after registration
+            messages.success(request, "Your account has been created!")
+            return redirect("profile")
+    else:
+        form = UserRegisterForm()
+    return render(request, "blog/register.html", {"form": form})
+
+
+# Profile view
+@login_required
+def profile(request):
+    if request.method == "POST":
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        if u_form.is_valid():
+            u_form.save()
+            messages.success(request, "Your profile has been updated!")
+            return redirect("profile")
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+
+    return render(request, "blog/profile.html", {"u_form": u_form})
--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -79,15 +79,10 @@ WSGI_APPLICATION = "django_blog.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "your_db_name",
-        "USER": "your_db_user",
-        "PASSWORD": "your_password",
-        "HOST": "localhost",
-        "PORT": "5432",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
